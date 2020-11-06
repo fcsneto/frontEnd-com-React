db55dd1d77cea79233f8fc935b89f5eb1a2ee7aa
Francisco Neto <fcnetocontato@gmail.com>
2020-11-06 20:59:41 -0300

    file-loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 
 import './App.css';
+import avatar from './assets/img.png';
 
 import Header from './components/Header';
 
@@ -14,6 +15,7 @@ function App(){
     return (
         <>
             <Header title="Projects"/>
+            <img width={300} src={avatar}/>
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
             </ul>
